c3b58bf5c3f65dcb0b4c567fc4f7692eb2b6d885
Vu Nguyen <vu008308@gmail.com>
2026-02-23 14:47:57 +0700

    Fix: Remove explicit version for flutter-gradle-plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,11 @@
-// 1. Khai báo các Plugin ở đầu file
 plugins {
+    // Giữ lại các bản đã fix ở bước trước
     id("com.android.application") version "8.7.0" apply false
-    
-    // Sửa 1.9.22 thành 1.8.22 theo đúng yêu cầu của thông báo lỗi
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    
+    // Sửa dòng này: Bỏ version để tránh lỗi "already on the classpath"
+    id("dev.flutter.flutter-gradle-plugin") apply false
 }
 
 allprojects {
@@ -16,7 +15,6 @@ allprojects {
     }
 }
 
-// 2. Cấu hình thư mục build (Đã sửa lại cho gọn và đúng chuẩn Kotlin DSL)
 rootProject.layout.buildDirectory.set(rootProject.layout.projectDirectory.dir("../../build"))
 
 subprojects {
