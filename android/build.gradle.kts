54f54037158bce4c42d092ba82434db8d9297e0c
Vu Nguyen <vu008308@gmail.com>
2026-02-23 14:30:16 +0700

    Fix: Configured root build.gradle.kts and resolved line ending warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+// 1. Khai báo các Plugin ở đầu file
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    // Đây là dòng quan trọng nhất để sửa lỗi của bạn
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,13 +14,13 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+// 2. Cấu hình thư mục build (Đã sửa lại cho gọn và đúng chuẩn Kotlin DSL)
+rootProject.layout.buildDirectory.set(rootProject.layout.projectDirectory.dir("../../build"))
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.layout.buildDirectory.set(rootProject.layout.buildDirectory.get().dir(project.name))
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
